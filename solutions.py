5e6ba4825b9959c8ad168d77e3f4e050d47c9836
ayushchaubeyds01 <ayushchaubeyds@gmail.com>
2026-08-05 09:56:48 +0530

    practice

--- solutions.py
+++ solutions.py
@@ -555,3 +555,14 @@
 #     main()
 
 
+# function to check factorial
+def main():
+    n=int(input("enter n:"))
+    print(fact(n))
+def fact(x):
+    fact=1
+    for i in range(1,x+1):
+        fact*=i
+    return fact
+if __name__=="__main__":
+    main()
